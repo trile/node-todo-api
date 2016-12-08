58d445728d115876be73444f2dfb5861060fa392
Tri Le <trile@users.noreply.github.com>
2016-12-08 20:32:24 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,13 @@ app.post('/todos', (req, res) => {
     );
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        (todos) => res.send({todos}),
+        (err) => res.status(400).send(err)
+    )
+});
+
 app.listen(PORT, () => {
     console.log('Starting server on port:', PORT);
 });
